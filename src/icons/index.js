fbabedfc7dd1f2634e15c269d52f2f8c8ec0843b
Kuziilab <971605209@qq.com>
2026-07-10 16:06:41 +0800

    cart with checkbox selection

--- src/icons/index.js
+++ src/icons/index.js
@@ -246,3 +246,8 @@ export const IconCraft = icon('IconCraft', [
   'M10 1v3',
   'M14 1v3'
 ])
+
+// 购物车
+export const IconCart = icon('IconCart', [
+  'M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6'
+])
